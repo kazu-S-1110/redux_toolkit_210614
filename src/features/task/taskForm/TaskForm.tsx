4e134ae0845c0067e667c40c8bd166a73f6c36d0
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-15 08:17:13 +0900

    useformを学習。めっちゃ便利！

--- src/features/task/taskForm/TaskForm.tsx
+++ src/features/task/taskForm/TaskForm.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import styles from './TaskForm.module.scss';
+import { useForm } from 'react-hook-form';
 import TextField from '@material-ui/core/TextField';
 
+type Inputs = {
+  taskTitle: string;
+};
+
 const TaskForm: React.FC = () => {
+  const { register, handleSubmit, reset } = useForm();
+  const handleCreate = (data: Inputs) => {
+    console.log(data);
+    reset(); //reset関数を発火することでテキストフィールドを空にできる。
+  };
   return (
     <div className={styles.root}>
-      <form className={styles.form} noValidate autoComplete="off">
+      <form
+        onSubmit={handleSubmit(handleCreate)} //react-hook-formの特徴！
+        className={styles.form}
+      >
         <TextField
           id="outlined-basic"
           label="New task"
           variant="outlined"
+          {...register('taskTitle')} //useformとの連携（バージョン７から書式違う）
           className={styles.text_field}
         />
       </form>
