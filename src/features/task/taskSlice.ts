e960df0f89f4f0db79bfb925d23cde3bf8ffd8a3
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-16 08:09:49 +0900

    TaskSlice内でcreateTaskを定義。

---
+++ src/features/task/taskSlice.ts
@@ -0,0 +1,59 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState, AppThunk } from '../../app/store';
+import { fetchCount } from './counterAPI';
+
+interface TaskState {
+  idCount: number; //taskが何個あるかを管理
+  tasks: { id: number; title: string; completed: boolean }[]; // storeに保存するtaskの一覧
+  selectedTask: { id: number; title: string; completed: boolean }; //どのtaskが選択されているか
+  isModalOpen: boolean;
+}
+
+const initialState: TaskState = {
+  idCount: 1,
+  tasks: [{ id: 1, title: 'Task A', completed: false }],
+  selectedTask: { id: 0, title: '', completed: false },
+  isModalOpen: false,
+};
+
+// The function below is called a thunk and allows us to perform async logic. It
+// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
+// will call the thunk with the `dispatch` function as the first argument. Async
+// code can then be executed and other actions can be dispatched. Thunks are
+// typically used to make async requests.
+// export const incrementAsync = createAsyncThunk(
+//   'counter/fetchCount',
+//   async (amount: number) => {
+//     const response = await fetchCount(amount);
+//     // The value we return becomes the `fulfilled` action payload
+//     return response.data;
+//   }
+// );
+
+export const taskSlice = createSlice({
+  name: 'task',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    createTask: (state, action) => {
+      state.idCount++;
+      const newTask = {
+        id: state.idCount,
+        title: action.payload,
+        completed: false,
+      };
+      state.tasks = [newTask, ...state.tasks];
+    },
+  },
+});
+
+export const { createTask } = taskSlice.actions;
+
+// The function below is called a selector and allows us to select a value from
+// the state. Selectors can also be defined inline where they're used instead of
+// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
+export const selectCount = (state: RootState) => state.counter.value;
+
+
+
+export default taskSlice.reducer;
